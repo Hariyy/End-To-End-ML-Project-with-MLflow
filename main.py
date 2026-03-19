9cfa2ee8f979eadf3906a3f69ac341f742a48bd2
Hariyy <harishjds23@gmail.com>
2026-03-19 20:18:57 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from CATProject import logger
 from CATProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from CATProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataValidationTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
 except Exception as e:
